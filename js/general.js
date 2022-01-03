e23f0bfda7cbfd49bce1ba665b430723f5c702e7
agonzg40 <agonzg40@estudiantes.unileon.es>
2022-01-03 19:40:15 +0100

    hecho la parte cambio de calendarios y tabla

--- js/general.js
+++ js/general.js
@@ -10,3 +10,17 @@ function closeNotification(){
     var el = document.getElementById('notificationPopup');
     el.style.display = 'none';
 }
+
+function changeTitleGeneral(){
+    var aux = "GPB CALENDAR - Calendario General";
+    document.getElementById('general').innerHTML = aux;
+    document.getElementById('weeks').innerHTML = "ww/mm";
+}
+
+function changeTitlePersonal(){
+    var aux = "GPB CALENDAR - Calendario Personal";
+    document.getElementById('general').innerHTML = aux;
+    document.getElementById('weeks').innerHTML = "ww/mm";
+}
+
+createCalendar()
