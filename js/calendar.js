b4118063ec101c5533fe822db87ba78c75d5c913
agonzg40 <agonzg40@estudiantes.unileon.es>
2022-01-12 21:28:24 +0100

    Ajustes en el calendario

---
+++ js/calendar.js
@@ -0,0 +1,26 @@
+function chargePage(){
+    var aux = "GPB CALENDAR - Calendario General";
+    document.getElementById('general').innerHTML = aux;
+    document.getElementById('weeks').innerHTML = "ww/mm";
+    //createCalendar();
+}
+
+function changeTitleGeneral(){
+    var aux = "GPB CALENDAR - Calendario General";
+    document.getElementById('general').innerHTML = aux;
+    document.getElementById('weeks').innerHTML = "ww/mm";
+}
+
+function changeTitlePersonal(){
+    var aux = "GPB CALENDAR - Calendario Personal";
+    document.getElementById('general').innerHTML = aux;
+    document.getElementById('weeks').innerHTML = "ww/mm";
+}
+
+function previousWeek(){
+
+}
+
+function nextWeek(){
+    
+}
