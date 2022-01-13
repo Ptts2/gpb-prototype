f3775b91d2325abd9c2781069f1257328205cf9a
agonzg40 <agonzg40@estudiantes.unileon.es>
2022-01-13 18:56:33 +0100

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,7 +1,7 @@
 var date = new Date();
     var pr = date.toString();
-    
     var month = date.getMonth()+1;
+    var year = date.getFullYear();
 
 function chargePage(){
 
@@ -162,13 +162,105 @@ function previousWeek(){
             monthS = "Dec";
             break;
     }
+
+    if(month == 12){
+        year = year-1;
+    }
     
-    var aux = "GPB CALENDAR - Calendario General";
-    document.getElementById('general').innerHTML = aux;
-    document.getElementById('weeks').innerHTML = day+" - "+day1+" de "+monthS+ " de "+pr.substring(11,15);
+    document.getElementById('weeks').innerHTML = day+" - "+day1+" de "+monthS+ " de "+year;
 
 }
 
 function nextWeek(){
+    var monthS = "";
+    var day = pr.substring(8,10), day1 = 0;
+
+    switch (pr.substring(0,3)){
+        case "Mon":
+            day1 = parseInt(day)+6;
+            day = day;
+            
+            break;
+        case "Tue":
+            day1 = parseInt(day)+5;
+            day = day-1;
+            
+            break;
+        case "Wed":
+            day1 = parseInt(day)+4;
+            day = day-2;
+            
+            break;
+        case "Thu":
+            day1 = parseInt(day)+3;
+            day = day-3;
+            
+            break;
+        case "Fri":
+            day1 = parseInt(day)+2;
+            day = day-4;
+            
+            break;
+        case "Sat":
+            day1 = parseInt(day)+1;
+            day = day-5;
+            
+            break;
+        case "Sun":
+            day1 = parseInt(day);
+            day = day-6;
+            
+            break;
+    }
+
+    if(month != 12){
+        month = month+1;
+    }else{
+        month = 1;
+    }
+
+    switch (month){
+        case 1:
+            monthS = "Jan";
+            break;
+        case 2:
+            monthS = "Feb";
+            break;
+        case 3:
+            monthS = "Mar";
+            break;
+        case 4:
+            monthS = "Apr";
+            break;
+        case 5:
+            monthS = "May";
+            break;
+        case 6:
+            monthS = "Jun";
+            break;
+        case 7:
+            monthS = "Jul";
+            break;
+        case 8:
+            monthS = "Aug";
+            break;
+        case 9:
+            monthS = "Sep";
+            break;
+        case 10:
+            monthS = "Oct";
+            break;
+        case 11:
+            monthS = "Nov";
+            break;
+        case 12:
+            monthS = "Dec";
+            break;
+    }
+
+    if(month == 1){
+        year = year+1;
+    }
     
+    document.getElementById('weeks').innerHTML = day+" - "+day1+" de "+monthS+ " de "+year;
 }
