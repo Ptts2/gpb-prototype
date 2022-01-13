d2009d6decb5e04852f428d2f9a001d617c93524
agonzg40 <agonzg40@estudiantes.unileon.es>
2022-01-13 18:03:52 +0100

    pruebas

--- js/calendar.js
+++ js/calendar.js
@@ -1,23 +1,72 @@
 function chargePage(){
+    var date = new Date();
     var aux = "GPB CALENDAR - Calendario General";
     document.getElementById('general').innerHTML = aux;
-    document.getElementById('weeks').innerHTML = "ww/mm";
+    document.getElementById('weeks').innerHTML = date.getMonth()+"/"+date.getDay();
+    
     //createCalendar();
 }
 
 function changeTitleGeneral(){
+    var date = new Date();
+    var month = "";
+    switch (date.getMonth()){
+        case 0:
+            month="Jan";
+            break;
+        case 1:
+            month="Feb";
+            break;
+        case 2:
+            month="Mar";
+            break;
+        case 3:
+            month="Abr";
+            break;
+        case 4:
+            month="May";
+            break;
+        case 5:
+            month="Jun";
+            break;
+        case 6:
+            month="Jul";
+            break;
+        case 7:
+            month="Ago";
+            break;
+        case 8:
+                month="Sep";
+                break;
+        case 4:
+                month="Oct";
+                break;
+        case 5:
+                month="Nov";
+                break;
+        case 6:
+                month="Dec";
+                break;         
+    }
+
+    alert(date.toString());
+
     var aux = "GPB CALENDAR - Calendario General";
     document.getElementById('general').innerHTML = aux;
-    document.getElementById('weeks').innerHTML = "ww/mm";
+    document.getElementById('weeks').innerHTML = date.getDay()+"de "+month;
+    
 }
 
 function changeTitlePersonal(){
+    var date = new Date();
     var aux = "GPB CALENDAR - Calendario Personal";
     document.getElementById('general').innerHTML = aux;
-    document.getElementById('weeks').innerHTML = "ww/mm";
+    document.getElementById('weeks').innerHTML = date.getMonth()+"/"+date.getDay();
 }
 
 function previousWeek(){
+    var today = new Date();
+    alert(today.getDay());
 
 }
 
