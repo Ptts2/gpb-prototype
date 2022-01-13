d1d16bb9099e16d793e85308ba7653a18c03a10a
Roberto <47633569+Ptts2@users.noreply.github.com>
2022-01-13 20:19:22 +0100

    sadfdafsda

--- js/calendar.js
+++ js/calendar.js
@@ -47,64 +47,13 @@ actividadesGenerales = [
 actsGenerales = document.getElementsByClassName('actGeneral');
 actsPersonales = document.getElementsByClassName('actPers');
 
-var date = new Date();
-var pr = date.toString();
-var month = date.getMonth()+1;
-var year = date.getFullYear();
-var day = pr.substring(8,10), day1 = 0;
-var contador = 0;
-
 
 function chargePage(){
-    contador = 0;
-    var date = new Date();
-    var pr = date.toString();
-    var day = pr.substring(8,10), day1 = 0;
-    var month = pr.substring(3,7);
 
-    switch (pr.substring(0,3)){
-        case "Mon":
-            day1 = parseInt(day)+6;
-            day = day;
-            
-            break;
-        case "Tue":
-            day1 = parseInt(day)+5;
-            day = day-1;
-            
-            break;
-        case "Wed":
-            day1 = parseInt(day)+4;
-            day = day-2;
-            
-            break;
-        case "Thu":
-            day1 = parseInt(day)+3;
-            day = day-3;
-            
-            break;
-        case "Fri":
-            day1 = parseInt(day)+2;
-            day = day-4;
-            
-            break;
-        case "Sat":
-            day1 = parseInt(day)+1;
-            day = day-5;
-            
-            break;
-        case "Sun":
-            day1 = parseInt(day);
-            day = day-6;
-            
-            break;
-
-    }
+    getWeek();
     var aux = "GPB CALENDAR - Calendario General";
     document.getElementById('general').innerHTML = aux;
-    document.getElementById('weeks').innerHTML = day+" - "+day1+" de "+month+ " de "+pr.substring(11,15);
     
-    //createCalendar();
 }
 
 function changeTitleGeneral(){
@@ -143,219 +92,46 @@ function changeTitlePersonal(){
 
 }
 
+var offset = 0;
+var today = new Date();
+var dayOfWeekOffset = today.getDay();
+
+
+function getWeek(offset) {
+  offset = offset || 0; 
+  var firstDay = new Date();
+  firstDay.setDate(firstDay.getDate() + 1 - dayOfWeekOffset + (offset * 7));
+
+  var lastDay = new Date(firstDay);
+  lastDay.setDate(lastDay.getDate() + 6);
+
+  console.log(makeDateString(firstDay), makeDateString(lastDay));
+
+  document.getElementById('weeks').innerHTML = makeDateString(firstDay) + ' - ' + makeDateString(lastDay);
+
+
+}
+
+function makeDateString(date) {
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1;
+    var y = date.getFullYear();
+  
+    var dateString = dd + '/'+ mm + '/'+ y;
+    return dateString;
+  
+}
+  
 function previousWeek(){
-    var monthS = "";
-    var day = pr.substring(8,10), day1 = 0;
 
-    if(contador ==0){
-
-    switch (pr.substring(0,3)){
-        case "Mon":
-            day1 = parseInt(day)+6;
-            day = day;
-            
-            break;
-        case "Tue":
-            day1 = parseInt(day)+5;
-            day = day-1;
-            
-            break;
-        case "Wed":
-            day1 = parseInt(day)+4;
-            day = day-2;
-            
-            break;
-        case "Thu":
-            day1 = parseInt(day)+3;
-            day = day-3;
-            
-            break;
-        case "Fri":
-            day1 = parseInt(day)+2;
-            day = day-4;
-            
-            break;
-        case "Sat":
-            day1 = parseInt(day)+1;
-            day = day-5;
-            
-            break;
-        case "Sun":
-            day1 = parseInt(day);
-            day = day-6;
-            
-            break;
-    }
-        contador = 1;
-    }
-
-    if(month != 1){
-        month = month-1;
-    }else{
-        month = 12;
-    }
-
-    switch (month){
-        case 1:
-            monthS = "Jan";
-            break;
-        case 2:
-            monthS = "Feb";
-            break;
-        case 3:
-            monthS = "Mar";
-            break;
-        case 4:
-            monthS = "Apr";
-            break;
-        case 5:
-            monthS = "May";
-            break;
-        case 6:
-            monthS = "Jun";
-            break;
-        case 7:
-            monthS = "Jul";
-            break;
-        case 8:
-            monthS = "Aug";
-            break;
-        case 9:
-            monthS = "Sep";
-            break;
-        case 10:
-            monthS = "Oct";
-            break;
-        case 11:
-            monthS = "Nov";
-            break;
-        case 12:
-            monthS = "Dec";
-            break;
-    }
-
-
-    day = day-7;
-    day1 = day1-7;
-
-    if(month == 12){
-        year = year-1;
-    }
-    
+    offset = offset - 1;
+    getWeek(offset);
     document.getElementById('weeks').innerHTML = day+" - "+day1+" de "+monthS+ " de "+year;
-
 }
 
 function nextWeek(){
-    var monthS = "";
-    if(contador == 0){
-    switch (pr.substring(0,3)){
-        case "Mon":
-            day1 = parseInt(day)+6;
-            day = day;
-            
-            break;
-        case "Tue":
-            day1 = parseInt(day)+5;
-            day = day-1;
-            
-            break;
-        case "Wed":
-            day1 = parseInt(day)+4;
-            day = day-2;
-            
-            break;
-        case "Thu":
-            day1 = parseInt(day)+3;
-            day = day-3;
-            
-            break;
-        case "Fri":
-            day1 = parseInt(day)+2;
-            day = day-4;
-            
-            break;
-        case "Sat":
-            day1 = parseInt(day)+1;
-            day = day-5;
-            
-            break;
-        case "Sun":
-            day1 = parseInt(day);
-            day = day-6;
-            
-            break;
-    }
-
-        contador = 1;
-    }
-
-    day = day+7;
-    day1 = day1+7;
-
+    offset = offset + 1;
+    getWeek(offset);
     
-
-    /*if(month != 12){
-        month = month+1;
-    }else{
-        month = 1;
-    }*/
-
-    switch (month){
-        case 1:
-            monthS = "Jan";
-            if(day == 31){
-                day1 = 6;
-            }else if(day == 30){
-                day1 = 5;
-            }else if(day == 29){
-                day1 = 4;
-            }else if(day == 28){
-                day1 = 3;
-            }else if(day == 27){
-                day1 = 2;
-            }else if(day == 26){
-                day1 = 1;
-            }
-            break;
-        case 2:
-            monthS = "Feb";
-            break;
-        case 3:
-            monthS = "Mar";
-            break;
-        case 4:
-            monthS = "Apr";
-            break;
-        case 5:
-            monthS = "May";
-            break;
-        case 6:
-            monthS = "Jun";
-            break;
-        case 7:
-            monthS = "Jul";
-            break;
-        case 8:
-            monthS = "Aug";
-            break;
-        case 9:
-            monthS = "Sep";
-            break;
-        case 10:
-            monthS = "Oct";
-            break;
-        case 11:
-            monthS = "Nov";
-            break;
-        case 12:
-            monthS = "Dec";
-            break;
-    }
-
-    if(month == 1){
-        year = year+1;
-    }
-    
-    document.getElementById('weeks').innerHTML = day+" - "+day1+" de "+monthS+ " de "+year;
 }
+
